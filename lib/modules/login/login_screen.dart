bf6d43b84e4a4dd27e9a8563a0ef769473d0dc92
AbdelrahmanHosni626 <abdohosny626@gmail.com>
2022-11-04 15:03:44 +0200

    login & register design

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,15 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/shared/components/components.dart';
+import '../register/register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          'Login Screen',
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    defaultFormField(
+                        controller: emailController,
+                        type: TextInputType.name,
+                        prefix: Icons.email,
+                        label: 'Email',
+                        validate: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email Must not be Empty';
+                          }
+                          return null;
+                        }),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    defaultFormField(
+                        controller: passwordController,
+                        type: TextInputType.visiblePassword,
+                        prefix: Icons.lock_outlined,
+                        label: 'Password',
+                        validate: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password Must not be Empty';
+                          }
+                          return null;
+                        }),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    defaultButton(
+                      function: ()
+                      {
+                        if(formKey.currentState!.validate())
+                          {
+                            print(emailController.text);
+                            print(passwordController.text);
+                        }
+                      },
+                      text: 'LOGIN',
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Don\'t have an account?',
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            navigateAndFinish(context, RegisterScreen());
+                          },
+                          child: const Text(
+                            'Register Now',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/modules/register/register_screen.dart
+++ lib/modules/register/register_screen.dart
@@ -1,15 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/modules/login/login_screen.dart';
+import 'package:shop_app/shared/components/components.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+  var phoneController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          'Register Screen',
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    defaultFormField(
+                      controller: nameController,
+                      type: TextInputType.name,
+                      prefix: Icons.person_outline_outlined,
+                      label: 'Name',
+                      validate: (value) {
+                        if (value!.isEmpty) {
+                          return 'Name Must not be Empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    defaultFormField(
+                      controller: emailController,
+                      type: TextInputType.name,
+                      prefix: Icons.email,
+                      label: 'Email',
+                      validate: (value) {
+                        if (value!.isEmpty) {
+                          return 'Email Must not be Empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    defaultFormField(
+                      controller: phoneController,
+                      type: TextInputType.phone,
+                      prefix: Icons.phone,
+                      label: 'Phone',
+                      validate: (value) {
+                        if (value!.isEmpty) {
+                          return 'Phone Must not be Empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    defaultFormField(
+                        controller: passwordController,
+                        type: TextInputType.visiblePassword,
+                        prefix: Icons.lock_outlined,
+                        label: 'Password',
+                        validate: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password Must not be Empty';
+                          }
+                          return null;
+                        }),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    defaultButton(
+                      function: () {
+                        if (formKey.currentState!.validate()) {
+                          navigateAndFinish(context, LoginScreen());
+                        }
+                      },
+                      text: 'LOGIN',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
